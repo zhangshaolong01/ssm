ae5d5e8fccda3781e81be748912f52a70383f7ee
ZhangShaoLong <zhangshaolong01@163.com>
2018-12-12 18:00:39 +0800

    s

--- ssm/src/main/java/com/bjgoodwill/web/UserController.java
+++ ssm/src/main/java/com/bjgoodwill/web/UserController.java
@@ -1,31 +1,30 @@
 package com.bjgoodwill.web;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bjgoodwill.entity.User;
 import com.bjgoodwill.service.IUserService;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private IUserService userService;
 	
-	@RequestMapping(value="/showname",method=RequestMethod.GET)
-	public String showUserName(@RequestParam("uid") int uid,HttpServletRequest request,Model model){
+	@ResponseBody
+	@RequestMapping(value="/showUserName",produces="text/html;charset=UTF-8")
+	public String showUserName(int uid){
 		User user = userService.getUserById(uid);
-		if(user != null){
-			request.setAttribute("name", user.getUserName());
-			model.addAttribute("mame", user.getUserName());
-			return "showName";
-		}
-		request.setAttribute("error", "没有找到该用户！");
-		return "error";
+		
+		JSONObject result = new JSONObject();
+		result.put("user", user);
+		System.out.println(result.toJSONString());
+		
+		return result.toJSONString();
 	}
 }
